a0f43d6bd25954790075a8ec9896618c7bc4a774
Anas Ben Messaoud <anasbenmessaoud2000@gmail.com>
2023-10-27 17:58:28 +0100

    Hello

--- App/main.js
+++ App/main.js
@@ -1,3 +1,18 @@
+const theBtn1 = document.getElementById('oneBtn');
+const theinput1 = document.getElementById('one');
+const theBtn2 = document.getElementById('twoBtn');
+const theinput2 = document.getElementById('two');
+const theBtn3 = document.getElementById('threeBtn');
+const theinput3 = document.getElementById('three');
+const theBtn4 = document.getElementById('foorBtn');
+const theinput4 = document.getElementById('foor');
+const theBtn5 = document.getElementById('fiveBtn');
+const theinput5 = document.getElementById('five');
+const theBtn6 = document.getElementById('sixBtn');
+const theinput6 = document.getElementById('six');
+const theBtn7 = document.getElementById('sevenBtn');
+const theinput7 = document.getElementById('seven');
+
 function addTask() {
   var taskInput = document.getElementById("task");
   var taskText = taskInput.value;
@@ -47,6 +62,10 @@ function addTask() {
   ul.appendChild(li);
   taskInput.value = "";
 }
+
+theBtn1.addEventListener('click', function() {
+  theBtn1.parentElement.innerHTML theinput1.value
+})
 
 function deleteTask(button) {
   var li = button.parentElement;
